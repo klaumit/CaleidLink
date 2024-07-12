1d0be6cc8dcc16793ff618f17e81989f4c07c96b
Klaus Mittermann <klaus.mittermann@test.com.uk>
2024-07-12 22:45:29 +0200

    Create version.h

---
+++ Source/version.h
@@ -0,0 +1,12 @@
+/*
+ * version.h
+ * All rights reserved. Copyright (C) 1998,1999 by NARITA Tomio.
+ * $Id: version.h,v 1.48 2003/11/17 03:54:03 nrt Exp $
+ */
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define VERSION		"v.2.11 (Nov.17,2003)"
+
+#endif /* __VERSION_H__ */
